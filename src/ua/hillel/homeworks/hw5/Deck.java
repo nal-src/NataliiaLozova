8168bd3eb7f650ce0fd120ab45d049b82720e67d
Nataliia Lozova <n.alimaxina@gmail.com>
2021-08-05 07:30:25 +0300

    Home Work 5 (#1)
    Co-authored-by: Nataliia Lozova <nataliialo@nataliialo-mbp.lan.wixpress.com>

---
+++ src/ua/hillel/homeworks/hw5/Deck.java
@@ -0,0 +1,38 @@
+package ua.hillel.homeworks.hw5;
+
+import java.util.Random;
+
+public class Deck {
+    private String[] cardValues = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"};
+    private String[] cardSuits = {"Hearts", "Clubs", "Diamonds", "Spades"};
+    private static int numOfCards = 52;
+    private static Card[] cards =  new Card[numOfCards];
+
+    public Deck() {
+        create();
+    }
+
+    public Card[] getCards() {
+        return cards;
+    }
+
+    public void shuffle() {
+        Random random = new Random();
+        for (int i = 0; i < 100; i++) {
+            int in = random.nextInt(numOfCards);
+            int out = random.nextInt(numOfCards);
+            Card card = cards[in];
+            cards[in] = cards[out];
+            cards[out] = card;
+        }
+    }
+
+    private void create() {
+        int count = 0;
+        for (String suit: cardSuits) {
+            for (String value: cardValues) {
+                cards[count++] = new Card(suit, value);
+            }
+        }
+    }
+}
