4bbced357ee11d19b4d2b8eabf6deec01937412b
Nataliia Lozova <nataliialo@wix.com>
2021-08-10 07:56:47 +0300

    Howework 7

---
+++ src/ua/hillel/homeworks/hw7/Main.java
@@ -0,0 +1,11 @@
+package ua.hillel.homeworks.hw7;
+
+public class Main {
+    public static void main(String[] args) {
+        Logger log = new Logger();
+        log.info("Test info");
+        log.debug("Test debug");
+        log.error("Test error");
+        log.warning("Test warning");
+    }
+}
