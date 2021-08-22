d8449c5137ac615440947e2338c05a2f7d41d9f4
Nataliia Lozova <n.alimaxina@gmail.com>
2021-08-22 16:35:56 +0300

    Home work 9

---
+++ src/ua/hillel/homeworks/hw9/Main.java
@@ -0,0 +1,12 @@
+package ua.hillel.homeworks.hw9;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Task 1");
+        Task1.run();
+        System.out.println("Task 2");
+        Task2.run();
+        System.out.println("Task 3");
+        Task3.run();
+    }
+}
