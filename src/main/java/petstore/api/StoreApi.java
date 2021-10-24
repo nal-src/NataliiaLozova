f02378bc590cd8dfba010735e497c39ea4bc43e1
Nataliia Lozova <n.alimaxina@gmail.com>
2021-10-24 13:12:18 +0300

    Home work 18

--- src/main/java/petstore/api/StoreApi.java
+++ src/main/java/petstore/api/StoreApi.java
@@ -13,10 +13,27 @@ public class StoreApi extends BaseApi {
         return call(request);
     }
 
-    public static Response createOrder() throws IOException {
+    public static Response createOrder(String body) throws IOException {
         request = requestJsonBuilder()
                 .url(fullUrl("/store/order"))
-                .post(requestBody(""))
+                .post(requestBody(body))
+                .build();
+
+        return call(request);
+    }
+
+    public static Response getOrderById(int id) throws IOException {
+        request = requestJsonBuilder()
+                .url(fullUrl("/store/order/" + id))
+                .build();
+
+        return call(request);
+    }
+
+    public static Response deleteOrderById(int id) throws IOException {
+        request = requestJsonBuilder()
+                .url(fullUrl("/store/order/" + id))
+                .delete()
                 .build();
 
         return call(request);
